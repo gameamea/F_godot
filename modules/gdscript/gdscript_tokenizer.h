90a4de5b9ff1872a4b4139ee96fcb5aeda6918b8
laurento <laurent@gameamea.com>
2019-11-10 18:27:46 +0100

    sync from frugs repo

--- modules/gdscript/gdscript_tokenizer.h
+++ modules/gdscript/gdscript_tokenizer.h
@@ -197,8 +197,7 @@ class GDScriptTokenizerText : public GDScriptTokenizer {
 			GDScriptFunctions::Function func; //function for built in functions
 			int warning_code; //for warning skip
 		};
-		int line, col;
-    int code_pos, code_len;
+		int line, col, code_pos, code_len;
 		TokenData() {
 			type = TK_EMPTY;
 			line = col = 0;
@@ -206,12 +205,12 @@ class GDScriptTokenizerText : public GDScriptTokenizer {
 		}
 	};
 
-	void _make_token(Token p_type, int p_code_len = 1);
+	void _make_token(Token p_type, int p_code_len);
 	void _make_newline(int p_indentation = 0, int p_tabs = 0);
-	void _make_identifier(const StringName &p_identifier, int p_code_len = 1);
-	void _make_built_in_func(GDScriptFunctions::Function p_func, int p_code_len = 1);
-	void _make_constant(const Variant &p_constant, int p_code_len = 1);
-	void _make_type(const Variant::Type &p_type, int p_code_len = 1);
+	void _make_identifier(const StringName &p_identifier, int p_code_len);
+	void _make_built_in_func(GDScriptFunctions::Function p_func, int p_code_len);
+	void _make_constant(const Variant &p_constant, int p_code_len);
+	void _make_type(const Variant::Type &p_type, int p_code_len);
 	void _make_error(const String &p_error);
 
 	String code;
@@ -224,11 +223,6 @@ class GDScriptTokenizerText : public GDScriptTokenizer {
 	int tk_rb_pos;
 	String last_error;
 	bool error_flag;
-	enum {
-		INDENT_NONE,
-		INDENT_SPACES,
-		INDENT_TABS,
-	} file_indent_type;
 
 #ifdef DEBUG_ENABLED
 	Vector<Pair<int, String> > warning_skips;
